4a1140d92e939a7608fbe47e3bf781c5a15c1060
hossam.mbakry <hossam.mbakry@etisalat.com>
2023-09-16 21:06:24 +0300

    add quran view and add quran details

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:islami_app_c9_sat_7pm/core/theme/application_theme.dart';
 import 'package:islami_app_c9_sat_7pm/layout/home_layout.dart';
+import 'package:islami_app_c9_sat_7pm/moduls/quran/quran_details.dart';
 import 'package:islami_app_c9_sat_7pm/splash_view.dart';
 
 void main() {
@@ -10,10 +12,14 @@ class MyApplication extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ApplicationTheme.lightTheme,
+      darkTheme: ApplicationTheme.darkTheme,
       initialRoute: SplashView.routeName,
       routes: {
         SplashView.routeName: (context) => SplashView(),
         HomeLayout.routeName: (context) => HomeLayout(),
+        QuranDetails.routeName: (context) => QuranDetails(),
       },
     );
   }
--- lib/moduls/quran/quran_view.dart
+++ lib/moduls/quran/quran_view.dart
@@ -1,17 +1,199 @@
 import 'package:flutter/material.dart';
+import 'package:islami_app_c9_sat_7pm/moduls/quran/quran_details.dart';
+import 'package:islami_app_c9_sat_7pm/moduls/quran/widget/quran_item.dart';
 
 class QuranView extends StatelessWidget {
-  const QuranView({super.key});
+  QuranView({super.key});
+
+  final List<String> suraNames = [
+    "الفاتحه",
+    "البقرة",
+    "آل عمران",
+    "النساء",
+    "المائدة",
+    "الأنعام",
+    "الأعراف",
+    "الأنفال",
+    "التوبة",
+    "يونس",
+    "هود",
+    "يوسف",
+    "الرعد",
+    "إبراهيم",
+    "الحجر",
+    "النحل",
+    "الإسراء",
+    "الكهف",
+    "مريم",
+    "طه",
+    "الأنبياء",
+    "الحج",
+    "المؤمنون",
+    "النّور",
+    "الفرقان",
+    "الشعراء",
+    "النّمل",
+    "القصص",
+    "العنكبوت",
+    "الرّوم",
+    "لقمان",
+    "السجدة",
+    "الأحزاب",
+    "سبأ",
+    "فاطر",
+    "يس",
+    "الصافات",
+    "ص",
+    "الزمر",
+    "غافر",
+    "فصّلت",
+    "الشورى",
+    "الزخرف",
+    "الدّخان",
+    "الجاثية",
+    "الأحقاف",
+    "محمد",
+    "الفتح",
+    "الحجرات",
+    "ق",
+    "الذاريات",
+    "الطور",
+    "النجم",
+    "القمر",
+    "الرحمن",
+    "الواقعة",
+    "الحديد",
+    "المجادلة",
+    "الحشر",
+    "الممتحنة",
+    "الصف",
+    "الجمعة",
+    "المنافقون",
+    "التغابن",
+    "الطلاق",
+    "التحريم",
+    "الملك",
+    "القلم",
+    "الحاقة",
+    "المعارج",
+    "نوح",
+    "الجن",
+    "المزّمّل",
+    "المدّثر",
+    "القيامة",
+    "الإنسان",
+    "المرسلات",
+    "النبأ",
+    "النازعات",
+    "عبس",
+    "التكوير",
+    "الإنفطار",
+    "المطفّفين",
+    "الإنشقاق",
+    "البروج",
+    "الطارق",
+    "الأعلى",
+    "الغاشية",
+    "الفجر",
+    "البلد",
+    "الشمس",
+    "الليل",
+    "الضحى",
+    "الشرح",
+    "التين",
+    "العلق",
+    "القدر",
+    "البينة",
+    "الزلزلة",
+    "العاديات",
+    "القارعة",
+    "التكاثر",
+    "العصر",
+    "الهمزة",
+    "الفيل",
+    "قريش",
+    "الماعون",
+    "الكوثر",
+    "الكافرون",
+    "النصر",
+    "المسد",
+    "الإخلاص",
+    "الفلق",
+    "الناس"
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "Quran View",
-        style: TextStyle(
-          fontSize: 30,
+    var theme = Theme.of(context);
+    return Column(
+      children: [
+        Image.asset(
+          "assets/images/quran_header.png",
         ),
-      ),
+        Divider(
+          thickness: 1.2,
+          color: theme.primaryColor,
+          endIndent: 10,
+          indent: 10,
+          height: 5,
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: Text(
+                "رقم السورة",
+                textAlign: TextAlign.center,
+                style: theme.textTheme.bodyMedium,
+              ),
+            ),
+            Container(
+              width: 1.2,
+              height: 40,
+              color: theme.primaryColor,
+            ),
+            Expanded(
+              child: Text(
+                "إسم السورة",
+                textAlign: TextAlign.center,
+                style: theme.textTheme.bodyMedium,
+              ),
+            ),
+          ],
+        ),
+        Divider(
+          thickness: 1.2,
+          color: theme.primaryColor,
+          endIndent: 10,
+          indent: 10,
+          height: 5,
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemBuilder: (context, index) => GestureDetector(
+              onTap: () {
+                Navigator.pushNamed(
+                  context,
+                  QuranDetails.routeName,
+                  arguments: SuraDetails(
+                      suraName: suraNames[index], suraNumber: "${index + 1}"),
+                );
+              },
+              child: QuranItem(
+                suraName: suraNames[index],
+                suraNumber: "${index + 1}",
+              ),
+            ),
+            itemCount: suraNames.length,
+          ),
+        )
+      ],
     );
   }
+}
+
+class SuraDetails {
+  final String suraName;
+  final String suraNumber;
+
+  SuraDetails({required this.suraName, required this.suraNumber});
 }
